5d8f72dec1c54b1075d13daec6c8d2444e55ffb7
Logan R. Kearsley <chronosurfer@gmail.com>
2012-05-01 13:30:20 -0600

    CPSed the interpreter

--- builtins.py
+++ builtins.py
@@ -1,8 +1,8 @@
 ### built-in globals
 
-from stypes import Env
-from seval import eval
+from stypes import Env, Tail
 from sparser import parse, to_string, isa, Symbol
+import operator
 
 class Closure():
 	def __init__(self, clos_env, vars, sym, body):
@@ -11,79 +11,96 @@ class Closure():
 		self.sym = sym
 		self.body = body
 
-	def __call__(self, call_env, *args):
+	def __call__(self, k, call_env, *args):
 		new_env = Env(zip(self.vars, args), self.clos_env)
 		new_env[self.sym] = call_env
 		if not 'self' in args: new_env['self'] = self #safe recursion
-		return eval(self.body, new_env)
+		return Tail(self.body, new_env, k)
 		
 	def __repr__(self):
 		return "vau (%s)"%(','.join(self.vars),)
 
-def defvar(v,var,e):
-	val = eval(e, v)
-	v[var] = val
-	return val
+def defvar(k,v,var,e):
+	def def_k(val):
+		v[var] = val
+		return k(val)
+	return Tail(e,v,def_k)
 	
-def setvar(v,var,e):
-	val = eval(e, v)
-	v.find(var)[var] = val
-	return val
+def setvar(k,v,var,e):
+	def set_k(val):
+		v.find(var)[var] = val
+		return k(val)
+	return Tail(e,v,set_k)
 
-def cond(v,*x):
-	for (p, e) in x:
-		if eval(p, v):
-			return eval(e, v)
-	raise ValueError("No Branch Evaluates to True")
+def cond(k,v,*x):
+	if len(x) == 0:
+		raise ValueError("No Branch Evaluates to True")
+	else:
+		(p, e) = x[0]
+		return Tail(p,v,lambda vp: Tail(e,v,k) if vp else cond(k,v,*x[1:]))
 
-def sequence(v,*x):
-	val = 0
-	for e in x:
-		val = eval(e, v)
-	return val
+def sequence(k,v,*x):
+	if len(x) == 0: return k(None)
+	return Tail(x[0],v,k if len(x) == 1 else lambda vx: sequence(k,v,*x[1:]))
 	
-def vprint(v,e):
-	val = eval(e, v)
-	print to_string(val)
-	return val
+def vprint(k,v,x):
+	def print_k(val):
+		print to_string(val)
+		return k(val)
+	return Tail(x,v,print_k)
 
-def wrap(v,p):
-	p = eval(p,v)
-	return lambda v,*x: p(v,*[eval(expr,v) for expr in x])
+def cps_map_eval(k,v,*x):
+	vx = []
+	def map_loop(mk,mv,*mx):
+		if len(mx) == 0: return k(vx)
+		else:
+			def assign_val(vmx):
+				vx.append(vmx)
+				return map_loop(mk,mv,*mx[1:])
+			return Tail(mx[0],v,assign_val)
+	return map_loop(k,v,*x)
+
+def wrap(k,v,p):
+	return Tail(p,v,
+		lambda vp:
+			k(lambda ck,cv,*x:
+				cps_map_eval(lambda vx: vp(ck,cv,*vx),cv,*x)))
+
+def make_cps_binop(op):
+	return lambda k,v,x,y:Tail(x,v,lambda vx: Tail(y,v, lambda vy: k(op(vx,vy))))
 
 basic_env = Env({
-	'+':	lambda v,x,y:eval(x,v)+eval(y,v),
-	'-':	lambda v,x,y:eval(x,v)-eval(y,v),
-	'*':	lambda v,x,y:eval(x,v)*eval(y,v),
-	'/':	lambda v,x,y:eval(x,v)/eval(y,v),
-	'>':	lambda v,x,y:eval(x,v)>eval(y,v),
-	'<':	lambda v,x,y:eval(x,v)<eval(y,v),
-	'>=':	lambda v,x,y:eval(x,v)>=eval(y,v),
-	'<=':	lambda v,x,y:eval(x,v)<=eval(y,v),
-	'=':	lambda v,x,y:eval(x,v)==eval(y,v),
-	'eq?':	lambda v,x,y:
-				(lambda vx,vy: (not isa(vx, list)) and (vx == vy))(eval(x,v),eval(y,v)),
-	'cons':	lambda v,x,y:[eval(x,v)]+eval(y,v),
-	'car':	lambda v,x:eval(x,v)[0],
-	'cdr':	lambda v,x:eval(x,v)[1:],
-	'list':	lambda v,*x:[eval(expr, v) for expr in x],
-	'append':	lambda v,x,y:eval(x,v)+eval(y,v),
-	'len':	lambda v,x:len(eval(x,v)),
-	'symbol?':	lambda v,x:isa(eval(x,v),Symbol),
-	'list?':	lambda v,x:isa(eval(x,v),list),
-	'atom?':	lambda v,x:not isa(eval(x,v), list),
-	'exit':		lambda v:exit(),
+	'+':	make_cps_binop(operator.add),
+	'-':	make_cps_binop(operator.sub),
+	'*':	make_cps_binop(operator.mul),
+	'/':	make_cps_binop(operator.div),
+	'>':	make_cps_binop(operator.gt),
+	'<':	make_cps_binop(operator.lt),
+	'>=':	make_cps_binop(operator.ge),
+	'<=':	make_cps_binop(operator.le),
+	'=':	make_cps_binop(operator.eq),
+	'eq?':	make_cps_binop(lambda vx,vy: (not isa(vx, list)) and (vx == vy)),
+	'cons':	make_cps_binop(lambda vx,vy: [vx]+vy),
+	'car':	lambda k,v,x:Tail(x,v, lambda vx: k(vx[0])),
+	'cdr':	lambda k,v,x:Tail(x,v, lambda vx: k(vx[1:])),
+	'list': cps_map_eval,
+	'append':	make_cps_binop(operator.add),
+	'len':	lambda k,v,x:Tail(x,v,lambda vx: k(len(vx))),
+	'symbol?':	lambda k,v,x:Tail(x,v,lambda vx: k(isa(vx,Symbol))),
+	'list?':	lambda k,v,x:Tail(x,v,lambda vx: k(isa(vx,list))),
+	'atom?':	lambda k,v,x:Tail(x,v,lambda vx: k(not isa(vx,list))),
+	'exit':		lambda k,v:exit(),
 	'#t':	True,
 	'#f':	False,
-	'if':		lambda v,z,t,f: eval((t if eval(z,v) else f), v),
+	'if':		lambda k,v,z,t,f: Tail(z,v,lambda vz: Tail((t if vz else f),v,k)),
 	'cond':	cond,
 	':=':	defvar,
 	'<-':	setvar,
-	'vau':	lambda v,args,sym,body: Closure(v,args,sym,body),
-	'quote': lambda v,x: x,
+	'vau':	lambda k,v,args,sym,body: k(Closure(v,args,sym,body)),
+	'quote': lambda k,v,x: k(x),
 	'seq': sequence,
 	'print': vprint,
-	'eval': lambda v,e,x: eval(eval(x,v),eval(e,v)),
+	'eval': lambda k,v,e,x: Tail(x,v,lambda vx: Tail(e,v,lambda ve: Tail(vx,ve,k))),
 	'wrap': wrap
 })
 
--- seval.py
+++ seval.py
@@ -1,16 +1,28 @@
 #### seval.py
 
+from stypes import Tail
 from sparser import to_string, Symbol, isa
 
 #### eval
 
-def eval(x, env):
+def eval(x, env, k=lambda x:x):
 	"Evaluate an expression in an environment."
-	if isa(x, Symbol):		  # variable reference
-		return env[x]
-	elif isa(x, list):		  # (proc exp*)
-		proc = eval(x[0], env)
-		if hasattr(proc, '__call__'):
-			return proc(env,*x[1:])
-		raise ValueError("%s = %s is not a procedure" % (to_string(x[0]),to_string(proc)))
-	return x
+	val = None
+	while True:
+		if isa(x, Symbol):		  # variable reference
+			val = k(env[x])
+		elif isa(x, list):		  # (proc exp*)
+			def capture_args():
+				cx, cv, ck = x[1:], env, k
+				def try_call(proc):
+					if hasattr(proc, '__call__'):
+						return proc(ck,cv,*cx)
+					raise ValueError("%s = %s is not a procedure" % (to_string(x[0]),to_string(proc)))
+				return try_call
+			x, k = x[0], capture_args()
+			continue
+		else:
+			val = k(x)
+		if not isa(val, Tail):
+			return val
+		(x,env,k) = val
