b63b5c420378f36f3b3d4a2002b1e76ba989d1cc
Logan Kearsley <chronosurfer@gmail.com>
2012-05-08 23:29:06 -0600

    improved efficiency of comment parsing

--- sparser.py
+++ sparser.py
@@ -8,11 +8,21 @@ def parse(tokens):
 		if '(' == token:
 			L = []
 			while tokens[0] != ')':
-				L.append(parse(tokens))
+				subtree = parse(tokens)
+				if subtree is not None: #edge case for empty comments
+					L.append(subtree)
 			tokens.pop(0) # pop off ')'
 			return L
 		elif ";" == token: #ignore the next full expression
-			parse(tokens)
+			token = tokens[0]
+			if ')' == token:
+				return None
+			tokens.pop(0)
+			parencount  = 1 if '(' == token else 0
+			while parencount > 0:
+				token = tokens.pop(0)
+				if '(' == token: parencount += 1
+				elif ')' == token: parencount -= 1
 			return parse(tokens) if len(tokens) > 0 else None
 		elif ')' == token:
 			raise SyntaxError('unexpected )')
