d79d81dca19cb6a7fddb99f913332b54cbd6ae60
Logan Kearsley <chronosurfer@gmail.com>
2012-05-10 22:43:10 -0600

    fixed error in the error checking

--- seval.py
+++ seval.py
@@ -31,16 +31,15 @@ def eval(x, env, k=lambda x:x):
 			if x.tag == 'sym':		# variable reference
 				val = k(env[x.value])
 			elif x.tag == 'list':		# (proc exp*)
-				xv = x.value
 				def capture_args():
-					cx, cv, ck = xv.cdr, env, k
+					cx, cv, ck = x.cdr, env, k
 					def try_call(proc):
 						vp = proc.value
 						if hasattr(vp, '__call__'):
 							return vp(ck,cv,*cx)
-						raise ValueError("%s = %s is not a procedure" % (to_string(xv[0]),to_string(proc)))
+						raise ValueError("%s = %s is not a procedure" % (to_string(x.car),to_string(proc)))
 					return try_call
-				x, k = xv.car, capture_args()
+				x, k = x.car, capture_args()
 				continue
 			else:
 				raise ValueError("Cannot evaluate type %s"%x.tag)
